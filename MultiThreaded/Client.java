52da4a63a597afec271938f8f15027ecad1364ea
narra-abhinay-sriram <121sriramreddy@gmail.com>
2025-04-16 01:04:04 +0530

    client code

---
+++ MultiThreaded/Client.java
@@ -0,0 +1,67 @@
+package MultiThreaded;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.Socket;
+
+public class Client {
+
+   public Runnable getRunnable() { 
+
+    return new Runnable() {
+        @Override
+        public void run(){
+
+            int port =8010;
+
+            try{
+
+                InetAddress address = InetAddress.getByName("localhost");
+
+                Socket socket= new Socket(address,port);
+                try{
+
+                    PrintWriter tosockeet = new PrintWriter(socket.getOutputStream());
+                    BufferedReader fromsocket = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                    tosockeet.println("hi from cleint");
+                    String line = fromsocket.readLine();
+                    System.out.println(line);
+                   
+                }catch(Exception ex)
+                {
+                    ex.printStackTrace();
+                }
+
+            }
+            catch(Exception ex)
+            {
+                ex.printStackTrace();
+            }
+
+        }
+    };
+
+   }
+
+    public static void main(String [] args)
+    {
+        Client client = new Client();
+
+        try{
+
+            for(int i=0;i<100;i++)
+            {
+                Thread thread = new Thread();
+                thread.start();
+            }
+
+        }
+        catch(Exception ex)
+        {
+            ex.printStackTrace();
+        }
+    }
+    
+}
